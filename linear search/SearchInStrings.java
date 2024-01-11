d2ef32b7dc4e08857d4626aa2edf3476d7662b14
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2024-01-11 10:51:14 +0530

    searching in strings

---
+++ linear search/SearchInStrings.java
@@ -0,0 +1,34 @@
+public class SearchInStrings {
+    public static void main(String[] args) {
+        String name = "Siddhanth";
+        char target = 'a';
+        System.out.println(search(name, target));
+    }
+
+    static boolean search2(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (char ch : str.toCharArray()) {
+            if (ch == target) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
