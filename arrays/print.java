0124c48a72cd3ea4b92ef94959e76126475f77fb
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2024-02-06 18:18:14 +0530

    array school level questions done

---
+++ arrays/print.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class print {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
---
+++ arrays/sum.java
@@ -0,0 +1,17 @@
+package arrays;
+
+class sum {
+    static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Sum of array elements: " + sumArray(arr));
+    }
+}
+
